a8cb7da6230d1aa82dade7ca80847cc83855641a
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2022-10-03 10:01:19 +0100

    started on page builder file

---
+++ src/modules/build-pages.js
@@ -0,0 +1,10 @@
+import { createH1, createH3, createBtn, creatrDiv, createPara, createIcon } from './dom';
+import { removeProject, renderTrashProjects } from './projects';
+import { renderTodos } from './todos';
+import { openEditProjectModal, openModal } from './modal';
+
+const buildInbox  = () => {
+    const nav = document.querySelector('.header');
+    const mainContainer = document.querySelector('.main-container');
+    
+};
